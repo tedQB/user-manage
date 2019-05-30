34e9efd6df7de777cc8498118a6fc7d1dd4d34e9
kapaidashu <kasia0001@126.com>
2019-05-30 22:41:40 +0800

    hide password

--- settings.ini.php
+++ settings.ini.php
@@ -1,6 +1,6 @@
 ;<?php return; ?>
 [SQL]
-host = xdm45835293.my3w.com
-user = xdm45835293
-password = aaa121914
-dbname = xdm45835293_db
+host = ****.***.com
+user = ****.***.
+password = ****.***.
+dbname = ****.***.
